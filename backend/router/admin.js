1ddc61eb2822a9fc5256dc01ccd603a175e6619e
orlinromy <orlinromy@gmail.com>
2022-08-03 14:41:58 +0800

    fix the admin router

--- backend/router/admin.js
+++ backend/router/admin.js
@@ -13,71 +13,71 @@ let refreshTokens = [];
 console.log(refreshTokens);
 
 router.post("/register", async (req, res) => {
-    try {
-      const admin = await Admin.findOne({ email: req.body.email });
-      if (admin) {
-        return res
-          .status(400)
-          .json({ status: "error", message: "email is taken" });
-      }
-  
-      const hash = await bcrypt.hash(req.body.password, 12);
-      const createdAdmin = await Admin.create({
-        email: req.body.email,
-        hash,
-      });
-  
-      console.log("successfully registered: ", createdAdmin);
-      res.json({ status: "ok", message: "successfully registered" });
-    } catch (error) {
-      console.log(error);
-      res.status(400).json({ status: "error", message: "an error has occured" });
+  try {
+    const admin = await Admin.findOne({ email: req.body.email });
+    if (admin) {
+      return res
+        .status(400)
+        .json({ status: "error", message: "email is taken" });
     }
-  });
+
+    const hash = await bcrypt.hash(req.body.password, 12);
+    const createdAdmin = await Admin.create({
+      email: req.body.email,
+      hash,
+    });
+
+    console.log("successfully registered: ", createdAdmin);
+    res.json({ status: "ok", message: "successfully registered" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: "error", message: "an error has occured" });
+  }
+});
 
 router.post("/login", async (req, res) => {
-    try {
-      const admin = await Admin.findOne({ email: req.body.email });
-  
-      if (!admin) {
-        return res
-          .status(400)
-          .json({ status: "error", message: "volunteer is not found" });
-      }
-  
-      const result = await bcrypt.compare(req.body.password, admin.hash);
-  
-      if (!result) {
-        console.log("email or password error");
-        return res
-          .status(401)
-          .json({ status: "error", message: "email or password error" });
-      }
-  
-      const payload = {
-        id: admin._id,
-        email: admin.email,
-      };
-  
-      const access = jwt.sign(payload, process.env.ACCESS_SECRET, {
-        expiresIn: "20m", 
-        jwtid: uuidv4(),
-      });
-  
-      const refresh = jwt.sign(payload, process.env.REFRESH_SECRET, {
-        expiresIn: "30d", 
-        jwtid: uuidv4(),
-      });
-      refreshTokens.push(refresh);
-      const response = { access, refresh };
-      console.log(refreshTokens);
-      res.json(response);
-    } catch (err) {
-      console.log("POST /login: ", err);
-      res.status(400).json({ status: "error", message: "login failed" });
+  try {
+    const admin = await Admin.findOne({ email: req.body.email });
+
+    if (!admin) {
+      return res
+        .status(400)
+        .json({ status: "error", message: "volunteer is not found" });
     }
-  });
 
-  router.patch("/match", async(req, res) => {
+    const result = await bcrypt.compare(req.body.password, admin.hash);
 
-  })
+    if (!result) {
+      console.log("email or password error");
+      return res
+        .status(401)
+        .json({ status: "error", message: "email or password error" });
+    }
+
+    const payload = {
+      id: admin._id,
+      email: admin.email,
+    };
+
+    const access = jwt.sign(payload, process.env.ACCESS_SECRET, {
+      expiresIn: "20m",
+      jwtid: uuidv4(),
+    });
+
+    const refresh = jwt.sign(payload, process.env.REFRESH_SECRET, {
+      expiresIn: "30d",
+      jwtid: uuidv4(),
+    });
+    refreshTokens.push(refresh);
+    const response = { access, refresh };
+    console.log(refreshTokens);
+    res.json(response);
+  } catch (err) {
+    console.log("POST /login: ", err);
+    res.status(400).json({ status: "error", message: "login failed" });
+  }
+});
+
+router.patch("/match", async (req, res) => {});
+
+module.exports = router;
