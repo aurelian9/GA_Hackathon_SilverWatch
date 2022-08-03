4c015a57435634fc1df68443a29ce9362da28df6
JibbyKT <89538905+JibbyKT@users.noreply.github.com>
2022-08-03 14:26:12 +0800

    seeded elderly pokemon

--- backend/router/elderly.js
+++ backend/router/elderly.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const Volunteer = require("../models/Volunteer");
 const Elderly = require("../models/Elderly");
 const auth = require("../middleware/auth");
+const seedElderly = require("./seedElderly");
 
 router.post("/list", auth, async (req, res) => {
   const elderList = await Volunteer.find({ _id: req.body._id }).select(
@@ -26,6 +27,21 @@ router.patch("/todo", auth, async (req, res) => {
   });
 
   res.json({ status: "ok", message: "updated" });
+});
+
+router.get("/seed", async (req, res) => {
+  await Elderly.deleteMany();
+
+  await Elderly.create(seedElderly, (err, data) => {
+    if (err) {
+      console.log("GET /seed error: " + err.message);
+      res
+        .status(400)
+        .json({ status: "error", message: "seeding error occurred" });
+    } else {
+      res.json({ status: "ok", message: "seeding successful" });
+    }
+  });
 });
 
 module.exports = router;
